ccaabb92ee39f2f17bc42002cd156b36a03c20e9
tiltproofRain <srijannr1@gmail.com>
2020-05-02 17:41:21 +0530

    fixed bug that lead to crash when loading the logo in main menu

--- game/MainMenu.lua
+++ game/MainMenu.lua
@@ -1,20 +1,19 @@
 local GameConstants = require('game/GameConstants')
 local MainMenu = {}
-local Resources = require('Resources')
+local Resources = require('game/Resources')
 
 local logoPos = {x = 0, y = 0}
 
 
 function MainMenu.load()
-  MainMenu.logo = love.graphics.newImage(Resources.Image.Logo)
-  local logoLen, logoWidth = MainMenu.logo:getDimensions()
+  local logoLen, logoWidth = Resources.Image.Logo:getDimensions()
   logoPos.x = (GameConstants.SCREEN_WIDTH - logoLen) / 2
   logoPos.y = (GameConstants.SCREEN_HEIGHT - logoWidth) / 2
 end
 
 
 function MainMenu.show()
-  love.graphics.draw(MainMenu.logo, logoPos.x, logoPos.y)
+  love.graphics.draw(Resources.Image.Logo, logoPos.x, logoPos.y)
 end
 
 
